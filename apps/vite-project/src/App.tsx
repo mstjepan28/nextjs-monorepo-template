775028390c0dead17b0ed9b67ba62bca1ac11136
Stjepan Markovčić <stjepan.markovcic@superius.hr>
2023-06-17 14:52:28 +0200

    .

--- apps/vite-project/src/App.tsx
+++ apps/vite-project/src/App.tsx
@@ -1,10 +1,10 @@
-import { CountButton } from '@shared/ui';
+// import { CountButton } from '@shared/ui';
+import './styles/styles.css';
 
 export const App = () => {
   return (
-    <div className="h-[100svh] flex flex-col justify-center items-center gap-y-4">
-      <h1 className="font-medium text-4xl">Vite + React</h1>
-      <CountButton />
+    <div className="appContainer">
+      123
     </div>
   )
 }
